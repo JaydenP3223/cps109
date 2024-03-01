ff11ebcd3e95959dfd46de5747e8e6243f3309e4
JaydenP3223 <150264456+JaydenP3223@users.noreply.github.com>
2024-03-01 10:13:18 -0500

    Update grades.py

--- assignments/grades.py
+++ assignments/grades.py
@@ -1,41 +1,40 @@
-grade = int(input('Enter your grade in number form: '))
+number_grade = int(input('Enter your grade in number form: '))
 
-if grade >= 93:
-    print(f'Your letter grade for {grade} is an A.')
+if number_grade >= 93:
+    letter_grade = 'A-'
 
-if grade >= 90 and grade <= 92:
-    print(f'Your letter grade for {grade} is an A-.')
+elif number_grade >= 90 and number_grade <= 92:
+    letter_grade = 'A-'
 
-if grade >= 87 and grade <= 89:
-    print(f'Your letter grade for {grade} is a B+.')
+elif number_grade >= 87 and number_grade <= 89:
+    letter_grade = 'B+'
 
-if grade >= 83 and grade <= 86:
-    print(f'Your letter grade for {grade} is a B.')
+elif number_grade >= 83 and number_grade <= 86:
+    letter_grade = 'B'
 
-if grade >= 80 and grade <= 82:
-    print(f'Your letter grade for {grade}  is a B-.')
+elif number_grade >= 80 and number_grade <= 82:
+    letter_grade = 'B-'
 
-if grade >= 77 and grade <= 79:
-    print(f'Your letter grade for {grade} is a C+.')
+elif number_grade >= 77 and number_grade <= 79:
+    letter_grade = 'C+'
 
-if grade >= 73 and grade <= 76:
-    print(f'Your letter grade for {grade} is a C.')
+elif number_grade >= 73 and number_grade <= 76:
+    letter_grade = 'C'
 
-if grade >= 70 and grade <= 72:
-    print(f'Your letter grade for {grade} is a C-.')
+elif number_grade >= 70 and number_grade <= 72:
+    letter_grade = 'C-'
 
-if grade >= 67 and grade <= 69:
-    print(f'Your letter grade for {grade} is a D+.')
+elif number_grade >= 67 and number_grade <= 69:
+    letter_grade = 'D+'
 
-if grade >= 63 and grade <= 66:
-    print(f'Your letter grade for {grade} is a D.')
+elif number_grade >= 63 and number_grade <= 66:
+    letter_grade = 'D'
 
-if grade >= 60 and grade <= 62:
-    print(f'Your letter grade for {grade} is a D-.')
+elif number_grade >= 60 and number_grade <= 62:
+    letter_grade = 'D-'
 
-if grade < 60:
-    print(f'Your letter grade for {grade} is a F.')
-
-
+else:
+    letter_grade = 'F'
+print(f'Your letter grade for {number_grade} is {letter_grade}.')
 
 
